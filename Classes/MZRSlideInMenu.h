984960bd07e884a0d7949b9f4ed2380a06ebf214
shams-ahmed <shamsahmed@me.com>
2014-05-07 13:59:47 +0100

    added set menu background color, close on selection option

--- Classes/MZRSlideInMenu.h
+++ Classes/MZRSlideInMenu.h
@@ -31,6 +31,9 @@
 /* allow to dismiss on background touched by user */
 @property (assign, nonatomic) BOOL dismissOnBackgroundTouch;
 
+/* close menu on selection, Default YES */
+@property (assign, nonatomic) BOOL closeMenuOnSelection;
+
 /* add horizontal effect */
 @property (assign, nonatomic) CGFloat horizontalTransitionEffect;
 
@@ -52,6 +55,9 @@
 
 /* add menu item with title & textColor & backgroundColor */
 - (void)addMenuItemWithTitle:(NSString *)title textColor:(UIColor *)textColor backgroundColor:(UIColor *)backgroundColor;
+
+/* add menu background color */
+- (void)setMenuBackgroundColor:(UIColor *)color;
 
 /* get button name */
 - (NSString *)buttonTitleAtIndex:(NSInteger)index;
